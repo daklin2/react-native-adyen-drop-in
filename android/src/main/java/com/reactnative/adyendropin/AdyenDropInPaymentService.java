fa080152334e9885b0acf6411eac9bc85ab64e27
Oleksandr Volkov <43185533+shortSparrow@users.noreply.github.com>
2023-02-08 17:46:20 +0200

    [CZECOM-5991] [Adyen] 'Error message' appears during payment on background. (#7)

--- android/src/main/java/com/reactnative/adyendropin/AdyenDropInPaymentService.java
+++ android/src/main/java/com/reactnative/adyendropin/AdyenDropInPaymentService.java
@@ -1,13 +1,9 @@
 package com.reactnative.adyendropin;
 
-import android.content.Intent;
-import android.util.Log;
-
 import androidx.annotation.NonNull;
 
 import com.adyen.checkout.components.ActionComponentData;
 import com.adyen.checkout.components.PaymentComponentState;
-import com.adyen.checkout.components.model.payments.response.Action;
 import com.adyen.checkout.dropin.service.DropInService;
 import com.adyen.checkout.dropin.service.DropInServiceResult;
 
@@ -26,8 +22,6 @@ public class AdyenDropInPaymentService extends DropInService {
         if (AdyenDropInPayment.INSTANCE != null) {
             AdyenDropInPayment.INSTANCE.handlePaymentSubmit(paymentComponentState);
         }
-        DropInServiceResult result = handleResponse(paymentComponentJson);
-        sendResult(result);
     }
 
     @Override
@@ -45,18 +39,6 @@ public class AdyenDropInPaymentService extends DropInService {
         sendResult(result);
     }
 
-    protected DropInServiceResult handleResponse (JSONObject jsonObject) {
-        DropInServiceResult result;
-        Action action = Action.SERIALIZER.deserialize(jsonObject);
-        if (jsonObject == null) {
-            result = new DropInServiceResult.Error();
-        } else if (isAction(jsonObject)) {
-            result = new DropInServiceResult.Action(action);
-        } else {
-            result = new DropInServiceResult.Finished(jsonObject.toString());
-        }
-        return result;
-    }
     public boolean isAction (JSONObject jsonObject) {
         return jsonObject.has("action");
     }
